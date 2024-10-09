0534437e8e40ce1039dc197931782244a8cf5e21
ishitaR919 <164413665+ishitaR919@users.noreply.github.com>
2024-10-09 00:48:24 +0000

    datatypes

---
+++ jstut/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "ishitarasgmail.com"
+var accountPassword ="12345"
+accountCity ="jaipur" //possible
+let accountState;//undefined
+
+
+accountPassword = "4"
+
+console.log(accountPassword);
+/*
+prefer not to use var
+because of block scope and functional scope
+*/ 
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
---
+++ jstut/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello")//we can not use it here but can on browser
+
+console.log(3+3);
+
+console.log("ishita");
+
+let name="ishita"
+let age = 19
+
+let isLoggedIn = false
+
+//number range ==> 2 to power 53
+//bigint 
+//string =>""
+//boolean => true/false
+//null //actuall stand alone value 
+//undefined =>value not assigned
+// symbo; =>unique
+
+//object 
+
+console.log(typeof age);
+
+console.log(typeof name);
+
+console.log(typeof null);//gives object
+
+
