faa0a4a957ce2b3f47992f6a79a9dbd976c3895b
ishitaR919 <164413665+ishitaR919@users.noreply.github.com>
2024-10-09 08:10:35 +0000

    conversion of datatypes

---
+++ jstut/03_conversionoperation.js
@@ -0,0 +1,34 @@
+let score = "33abc"
+let total 
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+console.log(typeof total);
+console.log(typeof (total));
+
+
+let valueIn = Number (score)
+console.log(typeof valueIn);
+console.log(valueIn);
+
+//"33"=>33
+//"33abc"=>NaN
+ //true=>1;false=>0
+
+ let isLoggedIn ="5"
+ let boolIsLoggedIn = Boolean(isLoggedIn)
+ console.log(boolIsLoggedIn);
+ 
+
+ // 1=>true ; "hitesh"=> true
+
+ let someNumber = 33
+
+
+ let stringNumber = String(someNumber)
+console.log(typeof(stringNumber));
+
+
+
