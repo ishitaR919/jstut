431f68c7921c2a97829feddf5ec6489af3aa73a4
ishitaR919 <164413665+ishitaR919@users.noreply.github.com>
2024-10-12 06:42:29 +0000

    conversion operator

--- jstut/03_conversionoperation.js
+++ jstut/03_conversionoperation.js
@@ -1,34 +1,63 @@
-let score = "33abc"
-let total 
+// let score = "33abc"
+// let total 
 
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-console.log(typeof total);
-console.log(typeof (total));
+// console.log(typeof total);
+// console.log(typeof (total));
 
 
-let valueIn = Number (score)
-console.log(typeof valueIn);
-console.log(valueIn);
+// let valueIn = Number (score)
+// console.log(typeof valueIn);
+// console.log(valueIn);
 
-//"33"=>33
-//"33abc"=>NaN
- //true=>1;false=>0
+// //"33"=>33
+// //"33abc"=>NaN
+//  //true=>1;false=>0
 
- let isLoggedIn ="5"
- let boolIsLoggedIn = Boolean(isLoggedIn)
- console.log(boolIsLoggedIn);
+//  let isLoggedIn ="5"
+//  let boolIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(boolIsLoggedIn);
  
 
- // 1=>true ; "hitesh"=> true
+//  // 1=>true ; "hitesh"=> true
 
- let someNumber = 33
+//  let someNumber = 33
 
 
- let stringNumber = String(someNumber)
-console.log(typeof(stringNumber));
+// let stringNumber = String(someNumber)
+//console.log(typeof(stringNumber));
+
+//******************OPERATIONSS*****************
+
+// let value = 3 
+// let negValue = -value
+// console.log(negValue);
+
+
+// let str1="hello"
+// let str2="ishita"
+// let str3 = str1 +str2 
+// console.log(str3);
+
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log(1+2+"2");//32
+// console.log("1"+2+2);//122
+
+// console.log(+true);
+// console.log(+"");
+
+//let num1,num2,num3
+//num1 = num2 = num3 = 2 +  2
+
+let gameCounter = 100 
+gameCounter++;
+console.log(gameCounter);
+ 
+
 
 
 
