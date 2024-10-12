46a314de3b4950639e8aaab003d64c829672dfe1
ishitaR919 <164413665+ishitaR919@users.noreply.github.com>
2024-10-12 18:31:10 +0000

    number and maths

---
+++ jstut/07_numAndmaths.js
@@ -0,0 +1,24 @@
+// const score = 400
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 123.8968
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++maths++++++++++++++++ 
+
+console.log(Math);
+console.log(Math.tan(0.7854));
+console.log(Math.abs(-231));
+
+// floor->min value ; ceil->max value; round 
+//max , min  , random
+console.log(Math.random());
+console.log((Math.random()*10+1));
