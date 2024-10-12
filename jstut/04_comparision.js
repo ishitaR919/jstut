223cabf6fa75a8216c1d6068a4ae144cd3557de4
ishitaR919 <164413665+ishitaR919@users.noreply.github.com>
2024-10-12 07:09:02 +0000

    comparision

---
+++ jstut/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2>1);
+// console.log(1==2);
+// console.log(1>2);
+
+
+//comparing  diff datatype
+//give NAN
+
+// console.log(null == 0);//false
+// console.log(null > 0);//false
+// console.log(null <= 0);//true
+
+//undefined
+// console.log(undefined == 0);//false
+// console.log(undefined  > 0);//false
+// console.log(undefined <= 0);//false
+
+//STRICT CHECK//'==='
+
+console.log("2"===2);//false
+
+
+
+
+
+
+
+
