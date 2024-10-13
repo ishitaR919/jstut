61ee9a1b94c5f16992aadf70f91ec20ee9462b2d
ishitaR919 <164413665+ishitaR919@users.noreply.github.com>
2024-10-13 04:59:58 +0000

    date and time

---
+++ jstut/08_dateandTime.js
@@ -0,0 +1,21 @@
+//date 
+//rep single moment //date is a object in tha javascript
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());//
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+
+
+let myCreatedDate  = new Date (2024,10,23)
+console.log(myCreatedDate.toDateString());
+
+console.log();
+
+
+
+
+//time
